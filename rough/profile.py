b694c7765e44c071c99605977ae7a0227495040c
kpjmcg <65646272+kpjmcg@users.noreply.github.com>
2022-08-17 18:03:55 +1000

    gen_rotational_profiles

--- rough/profile.py
+++ rough/profile.py
@@ -7,19 +7,25 @@ __all__ = ['Ra', 'Rms']
 import numpy as np
 from PIL import Image  
 
-# %% ..\01_profile.ipynb 9
-def Ra(im, axis = 0, norm = True):
+# %% ..\01_profile.ipynb 5
+def Ra(im, #Numpy array or arraylike
+       axis = 0, 
+       norm = True #Normalize the profile by subtracting the mean 
+      ):
     '''
-    Calculates Mean Absolute Roughness (Ra) along given axis. Calculated as the average deviation of absolute height values from the mean line of a given profile.
+    Calculates Mean Absolute Roughness (Ra) along given axis. Defined as the average deviation of absolute height values from the mean line.
     '''
     if norm:
         im = im - np.mean(im, axis = axis)
-    return np.mean(np.absolute(im))
+    return np.mean(np.absolute(im), axis = axis)
 
-# %% ..\01_profile.ipynb 11
-def Rms(im, axis = 0, norm = True):
+# %% ..\01_profile.ipynb 8
+def Rms(im, #Numpy array or array like
+        axis = 0, 
+        norm = True #Normalize the profile by subtracting the mean
+       ):
     '''
-    Calculates Root Mean Square Roughness (Rms) along given axis. Calculated as the root mean square of deviations of height from the mean line of a given profile. 
+    Calculates Root Mean Square Roughness (Rms) along given axis. Defined as the root mean square of deviations of height from the mean line of a given profile. 
     '''
     if norm:
         im = im - np.mean(im, axis = axis)
